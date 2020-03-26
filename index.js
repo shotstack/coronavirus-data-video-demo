5ee0b7544cb87cbef92210f5f68345f1fcf95508
Jeff Shillitto <jeff@shillitto.com>
2020-03-26 21:50:50 +1100

    Rename file and add labels, fades and timing adjustments

--- index.js
+++ index.js
@@ -21,18 +21,19 @@ let countries = [];
 let count = 0;
 
 const generateVideo = (country, cases) => {
-    let start = 0;
-    let videoLength = 0;
-    const VIDEO_TAIL_LENGTH = 5;
+    const VIDEO_TOP_LENGTH = 2;
+    const VIDEO_TAIL_LENGTH = 4;
     const FRAME_LENGTH = 0.04;
-    const COUNTER_LENGTH = FRAME_LENGTH * 1;
+    const COUNTER_LENGTH = FRAME_LENGTH * 2;
+
+    let start = VIDEO_TOP_LENGTH;
+    let videoLength = 0;
 
     let textClips = [];
-    const dateOffset = new Shotstack.Offset;
-    const countryOffset = new Shotstack.Offset;
 
     for (let i = 0; i < cases.length; i++) {
         const totalCases = cases[i].total_cases;
+        const totalDeaths = cases[i].total_deaths;
         const date = moment(cases[i].date).format('DD MMM YYYY');
 
         if (totalCases <= 0) {
@@ -51,11 +52,25 @@ const generateVideo = (country, cases) => {
         casesText
             .setStyle('future')
             .setText(Number(totalCases).toLocaleString())
-            .setSize('xx-large');
+            .setSize('xx-large')
+            .setOffset({ y: 0.5 });
 
         let casesClip = new Shotstack.Clip;
         casesClip
             .setAsset(casesText)
+            .setStart(Number(clipStart))
+            .setLength(Number(clipLength));
+
+        let deathsText = new Shotstack.TitleAsset;
+        deathsText
+            .setStyle('future')
+            .setText(Number(totalDeaths).toLocaleString())
+            .setSize('xx-large')
+            .setOffset({ y: -0.9 });
+
+        let deathsClip = new Shotstack.Clip;
+        deathsClip
+            .setAsset(deathsText)
             .setStart(Number(clipStart))
             .setLength(Number(clipLength));
 
@@ -64,7 +79,8 @@ const generateVideo = (country, cases) => {
             .setStyle('future')
             .setText(date)
             .setSize('small')
-            .setOffset(dateOffset.setY(-1));
+            .setPosition('bottom')
+            .setOffset({ y: -0.35 });
 
         let dateClip = new Shotstack.Clip;
         dateClip
@@ -75,43 +91,106 @@ const generateVideo = (country, cases) => {
         start = (Number(start) + Number(clipLength) + Number(FRAME_LENGTH));
 
         textClips.push(casesClip);
+        textClips.push(deathsClip);
         textClips.push(dateClip);
     };
 
-    let textTrack = new Shotstack.Track;
-    textTrack
+    let fadeIn = new Shotstack.Transition;
+    fadeIn.setIn('fade');
+
+    let dataTrack = new Shotstack.Track;
+    dataTrack
         .setClips(textClips);
 
     let countryText = new Shotstack.TitleAsset;
     countryText
-        .setStyle('future')
-        .setText(country)
+        .setStyle('chunkLight')
+        .setText(country.toUpperCase())
         .setSize('medium')
-        .setPosition('top');
+        .setPosition('top')
+        .setOffset({ y: 0.4 });
 
     let countryClip = new Shotstack.Clip;
     countryClip
         .setAsset(countryText)
-        .setStart(0)
-        .setLength(videoLength);
+        .setStart(0.5)
+        .setLength(videoLength - 0.5)
+        .setTransition(fadeIn);
 
     let covidText = new Shotstack.TitleAsset;
     covidText
         .setStyle('future')
-        .setText('COVID-19 CASES')
+        .setText('CORONAVIRUS (COVID-19) PANDEMIC')
         .setSize('x-small')
         .setPosition('top')
-        .setOffset(countryOffset.setY(-0.3));
+        .setOffset({ y: 0.1 });
 
     let covidClip = new Shotstack.Clip;
     covidClip
         .setAsset(covidText)
-        .setStart(0)
-        .setLength(videoLength);
+        .setStart(0.75)
+        .setLength(videoLength - 0.75)
+        .setTransition(fadeIn);
 
-    let countryTrack = new Shotstack.Track;
-    countryTrack
-        .setClips([countryClip, covidClip]);
+    let casesLabelText = new Shotstack.TitleAsset;
+    casesLabelText
+        .setStyle('chunkLight')
+        .setText('CONFIRMED CASES')
+        .setSize('x-small')
+        .setOffset({ y: 1 });
+
+    let casesLabelClip = new Shotstack.Clip;
+    casesLabelClip
+        .setAsset(casesLabelText)
+        .setStart(Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setLength(videoLength - Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setTransition(fadeIn);
+
+    let deathsLabelText = new Shotstack.TitleAsset;
+    deathsLabelText
+        .setStyle('chunkLight')
+        .setText('DEATHS')
+        .setSize('x-small')
+        .setOffset({ y: -0.4 });
+
+    let deathsLabelClip = new Shotstack.Clip;
+    deathsLabelClip
+        .setAsset(deathsLabelText)
+        .setStart(Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setLength(videoLength - Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setTransition(fadeIn);
+
+    let casesPlaceholderText = new Shotstack.TitleAsset;
+    casesPlaceholderText
+        .setStyle('future')
+        .setText('0')
+        .setSize('xx-large')
+        .setOffset({ y: 0.5 });
+
+    let casesPlaceholderClip = new Shotstack.Clip;
+    casesPlaceholderClip
+        .setAsset(casesPlaceholderText)
+        .setStart(Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setLength(1)
+        .setTransition(fadeIn);
+
+    let deathsPlaceholderText = new Shotstack.TitleAsset;
+    deathsPlaceholderText
+        .setStyle('future')
+        .setText('0')
+        .setSize('xx-large')
+        .setOffset({ y: -0.9 });
+
+    let deathsPlaceholderClip = new Shotstack.Clip;
+    deathsPlaceholderClip
+        .setAsset(deathsPlaceholderText)
+        .setStart(Number(VIDEO_TOP_LENGTH - 1 - FRAME_LENGTH))
+        .setLength(1)
+        .setTransition(fadeIn);
+
+    let fixedTextTrack = new Shotstack.Track;
+    fixedTextTrack
+        .setClips([countryClip, covidClip, casesLabelClip, deathsLabelClip, casesPlaceholderClip, deathsPlaceholderClip]);
 
     let backgroundVideo = new Shotstack.VideoAsset;
     backgroundVideo
@@ -120,8 +199,10 @@ const generateVideo = (country, cases) => {
     let backgroundClip = new Shotstack.Clip;
     backgroundClip
         .setAsset(backgroundVideo)
-        .setStart(0)
-        .setLength(videoLength);
+        .setStart(0.5)
+        .setLength(videoLength - 0.5)
+        .setTransition(fadeIn)
+        .setOpacity(0.1);
 
     let backgroundTrack = new Shotstack.Track;
     backgroundTrack
@@ -129,14 +210,14 @@ const generateVideo = (country, cases) => {
 
     let soundtrack = new Shotstack.Soundtrack;
     soundtrack
-        .setSrc('https://s3-ap-southeast-2.amazonaws.com/shotstack-assets/private/bad-news.mp3')
+        .setSrc('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/private/bad-news.mp3')
         .setEffect('fadeInFadeOut');
 
     let timeline = new Shotstack.Timeline;
     timeline
         .setBackground('#000000')
         .setSoundtrack(soundtrack)
-        .setTracks([countryTrack, textTrack, backgroundTrack]);
+        .setTracks([fixedTextTrack, dataTrack, backgroundTrack]);
 
     let output = new Shotstack.Output;
     output
