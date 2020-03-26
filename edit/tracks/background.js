ea14fbd86d54afed80475f69e7c76651553d049d
Jeff Shillitto <jeff@shillitto.com>
2020-03-26 23:04:38 +1100

    Refactor background clip

--- edit/tracks/background.js
+++ edit/tracks/background.js
@@ -1,23 +1,12 @@
 const Shotstack = require('shotstack-sdk');
+const backgroundClip = require('./clips/background');
 
 module.exports = (videoLength) => {
-    let fadeIn = new Shotstack.Transition;
-    fadeIn.setIn('fade');
-
-    let backgroundVideo = new Shotstack.VideoAsset;
-    backgroundVideo
-        .setSrc('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/private/virus.mp4');
-
-    let backgroundClip = new Shotstack.Clip;
-    backgroundClip
-        .setAsset(backgroundVideo)
-        .setStart(0.5)
-        .setLength(videoLength - 0.5)
-        .setTransition(fadeIn);
+    const clip = backgroundClip(videoLength);
 
     let backgroundTrack = new Shotstack.Track;
     backgroundTrack
-        .setClips([backgroundClip]);
+        .setClips([clip]);
 
     return backgroundTrack;
 }
---
+++ edit/tracks/clips/background.js
@@ -0,0 +1,19 @@
+const Shotstack = require('shotstack-sdk');
+
+module.exports = (videoLength) => {
+    let fadeIn = new Shotstack.Transition;
+    fadeIn.setIn('fade');
+
+    let backgroundVideo = new Shotstack.VideoAsset;
+    backgroundVideo
+        .setSrc('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/private/virus.mp4');
+
+    let backgroundClip = new Shotstack.Clip;
+    backgroundClip
+        .setAsset(backgroundVideo)
+        .setStart(0.5)
+        .setLength(videoLength - 0.5)
+        .setTransition(fadeIn);
+
+    return backgroundClip;
+}
