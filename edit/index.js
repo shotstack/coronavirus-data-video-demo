5e728d139f9fe8b3d4c29b2da0d36590788d8f7a
Jeff Shillitto <jeff@shillitto.com>
2020-03-28 13:16:19 +1100

    Refactor data to track file

--- edit/index.js
+++ edit/index.js
@@ -1,88 +1,9 @@
-const moment = require('moment');
 const Shotstack = require('shotstack-sdk');
 const timeline = require('./timeline');
 const output = require('./output');
 
 module.exports = (country, cases) => {
-    const VIDEO_TOP_LENGTH = 2;
-    const VIDEO_TAIL_LENGTH = 4;
-    const FRAME_LENGTH = 0.04;
-    const COUNTER_LENGTH = FRAME_LENGTH * 2;
-
-    let start = VIDEO_TOP_LENGTH;
-    let videoLength = 0;
-
-    let textClips = [];
-
-    for (let i = 0; i < cases.length; i++) {
-        const totalCases = cases[i].total_cases;
-        const totalDeaths = cases[i].total_deaths;
-        const date = moment(cases[i].date).format('DD MMM YYYY');
-
-        if (totalCases <= 0) {
-            continue;
-        }
-
-        let clipStart = parseFloat(start.toFixed(2));
-        let clipLength = COUNTER_LENGTH;
-
-        if (i === cases.length - 1) {
-            clipLength = VIDEO_TAIL_LENGTH;
-            videoLength = start + VIDEO_TAIL_LENGTH;
-        }
-
-        let casesText = new Shotstack.TitleAsset;
-        casesText
-            .setStyle('future')
-            .setText(Number(totalCases).toLocaleString())
-            .setSize('xx-large')
-            .setOffset({ y: 0.5 });
-
-        let casesClip = new Shotstack.Clip;
-        casesClip
-            .setAsset(casesText)
-            .setStart(Number(clipStart))
-            .setLength(Number(clipLength));
-
-        let deathsText = new Shotstack.TitleAsset;
-        deathsText
-            .setStyle('future')
-            .setText(Number(totalDeaths).toLocaleString())
-            .setSize('xx-large')
-            .setOffset({ y: -0.9 });
-
-        let deathsClip = new Shotstack.Clip;
-        deathsClip
-            .setAsset(deathsText)
-            .setStart(Number(clipStart))
-            .setLength(Number(clipLength));
-
-        let dateText = new Shotstack.TitleAsset;
-        dateText
-            .setStyle('future')
-            .setText(date)
-            .setSize('small')
-            .setPosition('bottom')
-            .setOffset({ y: -0.35 });
-
-        let dateClip = new Shotstack.Clip;
-        dateClip
-            .setAsset(dateText)
-            .setStart(Number(clipStart))
-            .setLength(Number(clipLength));
-
-        start = (Number(start) + Number(clipLength) + Number(FRAME_LENGTH));
-
-        textClips.push(casesClip);
-        textClips.push(deathsClip);
-        textClips.push(dateClip);
-    };
-
-    let dataTrack = new Shotstack.Track;
-    dataTrack
-        .setClips(textClips);
-
-    const videoTimeline = timeline([dataTrack], videoLength, country)
+    const videoTimeline = timeline(country, cases);
     const videoOutput = output('mp4', 'sd');
 
     let edit = new Shotstack.Edit;
--- edit/timeline.js
+++ edit/timeline.js
@@ -2,21 +2,20 @@ const Shotstack = require('shotstack-sdk');
 const soundtrack = require('./soundtrack');
 const backgroundVideo = require('./tracks/background');
 const fixedText = require('./tracks/fixed');
+const dataAnimation = require('./tracks/data');
 
-module.exports = (tracks, videoLength, country) => {
+module.exports = (country, cases) => {
     const color = '#000000';
     const soundtrackSrc = soundtrack('https://shotstack-assets.s3-ap-southeast-2.amazonaws.com/private/bad-news.mp3', 'fadeInFadeOut');
+    const [dataTrack, videoLength] = dataAnimation(cases);
     const backgroundTrack = backgroundVideo(videoLength);
     const fixedTextTrack = fixedText(country, videoLength);
-
-    tracks.push(fixedTextTrack);
-    tracks.push(backgroundTrack);
 
     let timeline = new Shotstack.Timeline;
     timeline
         .setBackground(color)
         .setSoundtrack(soundtrackSrc)
-        .setTracks(tracks);
+        .setTracks([dataTrack, fixedTextTrack, backgroundTrack]);
 
     return timeline;
 }
